a18718e67ceb6145c48f6e600239bb91d8f76d4a
MarijnBuitenweg <marijnbuitenweg@gmail.com>
2024-12-19 15:26:56 +0100

    Add rudimentary packet size validation

--- generator/BaseTypeGenerator.py
+++ generator/BaseTypeGenerator.py
@@ -73,6 +73,8 @@ Table 14-38: Payload Length Definition in FLRC Packet, page 124
 		for iPacket, packet_name in enumerate(packets.keys()):
 			
 			total_bytes = packet_to_size_in_bytes(packets[packet_name])
+			if total_bytes > 116:
+				raise ValueError("Packet is too large")
 			PACKET_NAME = CamelCaseToUpper(packet_name)
 
 			VARIABLE_NAME_TYPE = f"REM_PACKET_TYPE_{PACKET_NAME}".ljust(60)
